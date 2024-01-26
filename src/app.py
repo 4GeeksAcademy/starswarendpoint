a41b28d45b675a58937f764e9e7f5ff9f249132c
ricardodelimaaa <142522306+ricardodelimaaa@users.noreply.github.com>
2024-01-26 15:00:00 +0000

    favoritos

--- src/app.py
+++ src/app.py
@@ -9,6 +9,7 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, Favoritos, Planetas, Usuarios, Personaje
+import json
 #from models import Person
 
 app = Flask(__name__)
@@ -42,17 +43,40 @@ def handle_hello():
     resultados=list(map(lambda usuario: usuario.serialize(), todoslosusuarios))
     return jsonify(resultados),200
 
+@app.route('/usuarios/<int:idusuarios>', methods=['GET'])
+def getusuarios(idusuarios):
+    usuarios = Usuarios.query.filter_by(id=idusuarios).first()
+    if usuarios is None:
+        return jsonify({"msg": "no existe usuarios"}),404
+    return jsonify(usuarios.serialize()),200
+
 @app.route('/planetas', methods=['GET'])
 def getplaneta():
     todoslosplanetas=Planetas.query.all()
     resultados=list(map(lambda planetas: planetas.serialize(), todoslosplanetas))
     return jsonify(resultados),200
 
+@app.route('/planetas/<int:idplaneta>', methods=['GET'])
+def getplanetaid(idplaneta):
+    planeta = Planetas.query.filter_by(id=idplaneta).first()
+    if planeta is None:
+        return jsonify({"msg": "no existe planeta"}),404
+    return jsonify(planeta.serialize()),200
+
+
+
 @app.route('/personaje', methods=['GET'])
 def getpersonaje():
     todoslospersonaje=Personaje.query.all()
     resultados=list(map(lambda personaje: personaje.serialize(), todoslospersonaje))
     return jsonify(resultados),200
+
+@app.route('/personaje/<int:idpersonaje>', methods=['GET'])
+def getpersonajeid(idpersonaje):
+    personaje = Personaje.query.filter_by(id=idpersonaje).first()
+    if personaje is None:
+        return jsonify({"msg": "no existe personaje"}),404
+    return jsonify(personaje.serialize()),200
 
 @app.route('/favoritos', methods=['GET'])
 def getfavoritos():
@@ -61,6 +85,31 @@ def getfavoritos():
         return jsonify({"msg": "no hay favoritos"}), 404
     resultados=list(map(lambda favoritos: favoritos.serialize(), todoslosfavoritos))
     return jsonify(resultados),200
+
+@app.route('/favoritos/<int:idfavoritos>', methods=['GET','DELETE'])
+def getfavoritosid(idfavoritos):
+    favoritos = Favoritos.query.filter_by(id=idfavoritos).first()
+    if favoritos is None:
+        return jsonify({"msg": "no existe favoritos"}),404
+    if request.method == "GET" :
+        return jsonify(favoritos.serialize()),200
+    if request.method == "DELETE" :
+        db.session.delete(favoritos)
+        db.session.commit()
+        return jsonify({"msg": "favorito eliminar"}),404
+
+
+@app.route('/favoritos', methods=['POST'])
+def postfavoritos():
+    body=json.loads(request.data)
+    nuevofavorito= Favoritos(
+        user_id=body["user_id"],
+        personaje_id=body["personaje_id"],
+        planeta_id=body["planeta_id"]
+    )
+    db.session.add(nuevofavorito)
+    db.session.commit()
+    return jsonify({"msg": "favorito creado"}),201
 
 
 # this only runs if `$ python src/app.py` is executed
